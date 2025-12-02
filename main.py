dd079260a1580eb775bf04af37cb8ced60f44a07
jjahongir2197 <jjahongir2197@gmail.com>
2025-12-02 15:35:27 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+5 - misol
+numbers = lambda x: "nol" if x == 0 else ("musbat" if x > 0 else "manfiy")
+print(numbers(19))
+print(numbers(-2))
+print(numbers(0))
+
+6 - misol
+yuza = lambda asos, balandlik: 0.5 * asos * balandlik
+print(yuza(10, 5))
+print(yuza(7, 3))
+
+7 - misol
+juftlik = lambda x: True if x % 2 == 0 else False
+print(juftlik(12))
+print(juftlik(9))
+
+8 - misol
+taqqoslash = lambda x: x > 10
+print(taqqoslash(15))
+print(taqqoslash(8))
+
+9 - misol
+aniqlash = lambda s: "a" in s.lower()
+print(aniqlash("Ali"))
+print(aniqlash("Bob"))
+
+10 - misol
+str_bosh = lambda s: s.upper()
+print(str_bosh("salom"))
+print(str_bosh("dunyo"))
